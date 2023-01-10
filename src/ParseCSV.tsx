96b20f7f069da7dbecf5d852a91ad41ba5c42038
hkgnp <hkgnp@github.com>
2023-01-10 23:12:24 +0800

    [feat] added inline function

--- src/ParseCSV.tsx
+++ src/ParseCSV.tsx
@@ -1,12 +1,12 @@
-import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user';
-import Papa from 'papaparse';
-import { useEffect, useState } from 'react';
-import ProgressBar from './ProgressBar';
+import { BlockEntity } from "@logseq/libs/dist/LSPlugin.user";
+import Papa from "papaparse";
+import React, { useEffect, useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 const uniqueIdentifier = () =>
   Math.random()
     .toString(36)
-    .replace(/[^a-z]+/g, '');
+    .replace(/[^a-z]+/g, "");
 
 const ParseCSV = () => {
   const [selectedFile, setSelectedFile] = useState();
@@ -21,72 +21,98 @@ const ParseCSV = () => {
 
   const parse = async (e) => {
     if (!isFilePicked) {
-      logseq.App.showMsg('Please select a file first.', 'error');
+      logseq.UI.showMsg("Please select a file first.", "error");
       return;
-    }
+    } else {
+      Papa.parse(selectedFile!, {
+        complete: async (results) => {
+          const response: any[] = results.data;
+          console.log(response);
 
-    Papa.parse(selectedFile, {
-      complete: async (results) => {
-        const response: any[] = results.data;
-
-        if (e.target.name === 'rendered') {
-          await logseq.Editor.insertAtEditingCursor(
-            `{{renderer :tables_${uniqueIdentifier()}}}`
-          );
-
-          const tableBlock = await logseq.Editor.getCurrentBlock();
-
-          const tableOptions = await logseq.Editor.insertBlock(
-            tableBlock.uuid,
-            'data nosum nostyle',
-            { before: false, sibling: false }
-          );
-
-          for (let r of response[0]) {
-            await logseq.Editor.insertBlock(tableOptions.uuid, r, {
-              before: false,
-              sibling: false,
-            });
-          }
-
-          const tableOptionsBlock: BlockEntity = await logseq.Editor.getBlock(
-            tableOptions.uuid,
-            { includeChildren: true }
-          );
-
-          const interval: number = 100 / tableOptionsBlock.children.length;
-          for (let i = 0; i < tableOptionsBlock.children.length; i++) {
-            setProgressPercentage(
-              (progressPercentage) => progressPercentage + interval
+          if (e.target.name === "rendered") {
+            await logseq.Editor.insertAtEditingCursor(
+              `{{renderer :tables_${uniqueIdentifier()}}}`
             );
-            for (let j = 1; j < response.length; j++) {
-              await logseq.Editor.insertBlock(
-                tableOptionsBlock.children[i]['uuid'],
-                response[j][i],
-                { before: false, sibling: false }
-              );
+
+            const tableBlock = await logseq.Editor.getCurrentBlock();
+
+            const tableOptions = await logseq.Editor.insertBlock(
+              tableBlock!.uuid,
+              "data nosum nostyle",
+              { before: false, sibling: false }
+            );
+
+            for (let r of response[0]) {
+              await logseq.Editor.insertBlock(tableOptions.uuid, r, {
+                before: false,
+                sibling: false,
+              });
             }
-          }
-        } else if (e.target.name === 'markdown') {
-          let markdownTable = '';
-          for (let r of response) {
-            let tmpVar = `|${r.join('|')}|`;
-            markdownTable = `${markdownTable}
+
+            const tableOptionsBlock: BlockEntity = await logseq.Editor.getBlock(
+              tableOptions.uuid,
+              {
+                includeChildren: true,
+              }
+            );
+
+            const interval: number = 100 / tableOptionsBlock.children.length;
+            for (let i = 0; i < tableOptionsBlock.children.length; i++) {
+              setProgressPercentage(
+                (progressPercentage) => progressPercentage + interval
+              );
+              for (let j = 1; j < response.length; j++) {
+                await logseq.Editor.insertBlock(
+                  tableOptionsBlock.children[i]["uuid"],
+                  response[j][i],
+                  { before: false, sibling: false }
+                );
+              }
+            }
+          } else if (e.target.name === "markdown") {
+            let markdownTable = "";
+            for (const r of response) {
+              let tmpVar = `|${r.join("|")}|`;
+              markdownTable = `${markdownTable}
 ${tmpVar}`;
+            }
+            await logseq.Editor.insertAtEditingCursor(markdownTable);
+          } else if (e.target.name === "inline") {
+            const blk = await logseq.Editor.getCurrentBlock();
+
+            let pBlk: any;
+            for (let i = 1; i < response.length; i++) {
+              for (let j = 0; j < response[i].length; j++) {
+                console.log(response[i][j]);
+                if (j === 0) {
+                  pBlk = await logseq.Editor.insertBlock(
+                    blk!.uuid,
+                    response[i][j],
+                    { before: true, sibling: true }
+                  );
+                } else {
+                  await logseq.Editor.insertBlock(pBlk.uuid, response[i][j], {
+                    before: false,
+                    sibling: false,
+                  });
+                }
+              }
+            }
+
+            await logseq.Editor.removeBlock(blk!.uuid);
           }
-          await logseq.Editor.insertAtEditingCursor(markdownTable);
-        }
-        setKeyValue(Date.now());
-        setIsFilePicked(false);
-        logseq.hideMainUI();
-        await logseq.Editor.restoreEditingCursor();
-      },
-    });
+          setKeyValue(Date.now());
+          setIsFilePicked(false);
+          logseq.hideMainUI();
+          await logseq.Editor.restoreEditingCursor();
+        },
+      });
+    }
   };
 
   return (
     <div className="flex justify-center border" tabIndex={-1}>
-      <div className="absolute top-10 bg-white rounded-lg p-3 w-1/3 border flex flex-col">
+      <div className="absolute top-10 bg-white rounded-lg p-3 w-1/2 border flex flex-col">
         <input
           key={keyValue}
           className="mt-5 mb-3 block w-full cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 focus:outline-none focus:border-transparent text-sm rounded-lg"
@@ -109,6 +135,14 @@ ${tmpVar}`;
             onClick={parse}
           >
             To Markdown Table
+          </button>
+          <button
+            name="inline"
+            type="button"
+            className="text-sm py-2 px-4 bg-green-800 hover:bg-blue-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+            onClick={parse}
+          >
+            Inline
           </button>
         </div>
         <ProgressBar progressPercentage={progressPercentage} />
--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const ProgressBar = ({ progressPercentage }) => {
-  return (
-    <React.Fragment>
-      {progressPercentage !== 0 && (
-        <p className="text-right text-sm">{progressPercentage.toFixed(2)}%</p>
-      )}
-      {progressPercentage !== 0 && (
-        <div className="h-2 w-full bg-gray-300">
-          <div
-            style={{ width: `${progressPercentage}%` }}
-            className={`h-full ${
-              progressPercentage < 70 ? 'bg-yellow-300' : 'bg-green-600'
-            }`}
-          ></div>
-        </div>
-      )}
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            {progressPercentage !== 0 && (
+                <p className="text-right text-sm">
+                    {progressPercentage.toFixed(2)}%
+                </p>
+            )}
+            {progressPercentage !== 0 && (
+                <div className="h-2 w-full bg-gray-300">
+                    <div
+                        style={{ width: `${progressPercentage}%` }}
+                        className={`h-full ${
+                            progressPercentage < 70
+                                ? "bg-yellow-300"
+                                : "bg-green-600"
+                        }`}
+                    ></div>
+                </div>
+            )}
+        </React.Fragment>
+    );
 };
 
 export default ProgressBar;
